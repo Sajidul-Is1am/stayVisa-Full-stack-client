92c7f7523bc5cce7204d34a40afa98e0de8b5ddb
MD Sajid <mds040772@gmail.com>
2023-12-06 21:56:03 +0600

    userInfo added on database

---
+++ src/api/Utils.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const ImageUpload = async image => {
+    const formdata = new FormData();
+    formdata.append("image", image);
+    const {
+        data
+    } = await axios.post(
+        `https://api.imgbb.com/1/upload?key=${
+          import.meta.env.VITE_IMABB_API_KEY
+        }`,
+        formdata
+    );
+    console.log(data);
+    return data
+}
