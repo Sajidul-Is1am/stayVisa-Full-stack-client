e23bb8cd3a03e8996fb6cce61a1d673076ab0842
MD Sajid <mds040772@gmail.com>
2023-12-08 19:52:53 +0600

    interceptor added on axiosSecure

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,23 @@
 import axios from "axios";
+import {
+    clearToken
+} from "./auth";
 
 
 
 const axiosSecure = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
-    withCredentials:true,
+    withCredentials: true,
+})
+
+
+// make interceptior response for unAuthorized responses
+axiosSecure.interceptors.response.use(response => response, async (error) => {
+    if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+        await clearToken()
+        window.location.replace('/login')
+    }
+    return Promise.reject(error)
 })
 
 export default axiosSecure
